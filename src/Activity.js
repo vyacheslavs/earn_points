6ca745d604040be758699adc66a6875018a5158b
Viacheslav Shegai <vshegai@epiphan.com>
2024-05-18 11:09:44 -0400

    Add activity and board

---
+++ src/Activity.js
@@ -0,0 +1,5 @@
+import './Activity.css';
+
+export default function Activity({name, amount}) {
+    return (<div class="activity">{name}</div>);
+}
---
+++ src/Board.js
@@ -0,0 +1,12 @@
+import './Board.css';
+import Activity from './Activity.js';
+import data from './activities.json';
+
+export default function Board() {
+        return (
+            <>
+            {data.activities.map((item, index) => (
+                <Activity name={item.name} amount={item.amount}></Activity>))}
+            </>
+        );
+}
