d3ef523e2b69d1070c3a76bba6af13236eb4b835
Viacheslav Shegai <vshegai@epiphan.com>
2024-05-18 19:25:13 -0400

    start working on history board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Board from './Board.js';
+import HistoryBoard from './HistoryBoard.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HistoryBoard/>
         <div className='board-container'>
           <Board></Board>
         </div>
---
+++ src/HistoryBoard.js
@@ -0,0 +1,7 @@
+
+const server = process.env.REACT_APP_BACKEND ?? "http://localhost:3001";
+
+export default function HistoryBoard() {
+    return (<div>Total points: </div>);
+}
+
