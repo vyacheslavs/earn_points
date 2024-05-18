1e0ac60130b432a1013fa758610b25e24ba4c5ba
Viacheslav Shegai <vshegai@epiphan.com>
2024-05-18 13:33:01 -0400

    Add mui

--- src/Activity.js
+++ src/Activity.js
@@ -1,19 +1,63 @@
+import * as React from 'react';
 import './Activity.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import IconButton from '@mui/material/IconButton';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { pink } from '@mui/material/colors';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
 library.add(fas)
 
-export default function Activity({name, amount, icon, disabled}) {
+export default function Activity({activity_data}) {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
     return (
-    <div className={"activity " + (disabled ? 'disabled' : 'enabled')}>
-        <div><FontAwesomeIcon className='icon' icon={icon} /></div>
+    <div className={"activity " + (activity_data.disabled ? 'disabled' : 'enabled')}>
+        <div><FontAwesomeIcon className='icon' icon={activity_data.icon} /></div>
         <div></div>
         <div className='name-container'>
-            <div className='name'>{name}</div>
-            <div>points: {amount}</div>
+            <div className='name'>{activity_data.name}</div>
+            <div>points: {activity_data.amount}</div>
         </div>
-        
+        <div className='help'>
+            <IconButton onClick={handleClickOpen}>
+                <HelpOutlineIcon sx={{ color: pink[500] }}/>
+            </IconButton>
+        </div>
+
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+        <DialogTitle id="alert-dialog-title">
+          {activity_data.help_caption}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {activity_data.help}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>OK</Button>
+        </DialogActions>
+      </Dialog>
     </div>);
 }
--- src/Board.js
+++ src/Board.js
@@ -6,7 +6,7 @@ export default function Board() {
         return (
             <>
             {data.activities.map((item, index) => (
-                <Activity key={index} name={item.name} amount={item.amount} icon={item.icon} disabled={item.disabled}></Activity>))}
+                <Activity key={index} activity_data={item}></Activity>))}
             </>
         );
 }
