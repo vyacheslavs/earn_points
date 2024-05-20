9cea4e36647412d4b98a8845d90cfc46875a1197
Viacheslav Shegai <a@ldd.so>
2024-05-20 08:25:40 -0400

    spend confirmation implemented

--- src/Spend.js
+++ src/Spend.js
@@ -20,7 +20,8 @@ export default function Spend() {
     const [act, setAct] = React.useState("Friend come over");
     const [actAmount, setActAmount] = React.useState(50);
     const [open, setOpen] = React.useState(false);
-    const [errorMsg, setErrorMsg] = React.useState("");
+    const [promptOpen, setPromptOpen] = React.useState(false);
+    const [alertMsg, setAlertMsg] = React.useState("");
     const {setHistoryBoardData} = React.useContext(HistoryBoardContext);
     
     const setSpendActivity = (amount, desc) => {
@@ -28,8 +29,15 @@ export default function Spend() {
         setActAmount(amount);
     };
 
+    const askBeforeSpend = async() => {
+        setPromptOpen(true);
+    };
+
     const doSpend = async () => {
         try {
+
+            setPromptOpen(false);
+
             const data = {"amount": actAmount, "name": act};
             const response = await axios.post(server + '/spend', data, {
                 headers: {
@@ -38,8 +46,8 @@ export default function Spend() {
             });
 
             if (!response.data.success) {
-                setErrorMsg(response.data.error);
-                handleClickOpen();    
+                setAlertMsg(response.data.error);
+                showAlertDialog();    
             } else {
                 // update history
                 const h = await updateBoardContext();
@@ -49,13 +57,13 @@ export default function Spend() {
             return response.data;
         } catch (error) {
             console.error('There was an error getting the data!', error);
-            setErrorMsg(error.message);
-            handleClickOpen();
+            setAlertMsg(error.message);
+            showAlertDialog();
             return {};
         }
     };
 
-    const handleClickOpen = () => {
+    const showAlertDialog = () => {
         setOpen(true);
     };
 
@@ -70,6 +78,10 @@ export default function Spend() {
     const onActivityNameChange = (event) => {
         setAct(event.target.value);
     };
+
+    const handlePromptClose = () => {
+        setPromptOpen(false);
+    }
     
 
     return (
@@ -82,7 +94,7 @@ export default function Spend() {
                 <TextField id="points" label="Amount of points to spend" variant="outlined" type="number" InputLabelProps={{shrink: true }} value={actAmount} onChange={onPointsChange}/>
             </div>
             <div className='control'>
-                <Button variant="contained" onClick={doSpend}>Spend</Button>
+                <Button variant="contained" onClick={askBeforeSpend}>Spend</Button>
             </div>
             <div className='control'>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }} />
@@ -105,15 +117,34 @@ export default function Spend() {
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            {errorMsg}
+            {alertMsg}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} autoFocus>
-            OK
-          </Button>
+          <Button onClick={handleClose} autoFocus>OK</Button>
         </DialogActions>
       </Dialog>
+
+      <Dialog
+        open={promptOpen}
+        onClose={handlePromptClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Spend some points?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Do you want to spend {actAmount} points on "{act}" activity?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handlePromptClose} autoFocus>Cancel</Button>
+          <Button onClick={doSpend}> OK</Button>
+        </DialogActions>
+      </Dialog>
+
     </Box>
     );
 }
