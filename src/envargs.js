a63f66b300173ccd928722ee53befb8260ec35be
Viacheslav Shegai <a@ldd.so>
2024-05-22 05:55:51 -0400

    fix REACT_APP_BACKEND env variable

--- src/envargs.js
+++ src/envargs.js
@@ -1,6 +1,6 @@
 const envVars = {
     secret : process.env.REACT_APP_BONUS_REWARD_SECRET || "",
-    server : process.env.REACT_APP_BACKEND || "http://localhost:3001"
+    server : process.env.REACT_APP_BACKEND === undefined ? "http://localhost:3001" : process.env.REACT_APP_BACKEND
 };
 
 export default envVars;
